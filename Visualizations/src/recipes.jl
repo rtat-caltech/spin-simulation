2cf0a8f17a34b16a50c8ab380ebb25b0eba9ef4a
Raymond Tat <tat.raymond5@gmail.com>
2021-07-07 16:12:21 -0700

    Make visualizations more modular

--- Visualizations/src/recipes.jl
+++ Visualizations/src/recipes.jl
@@ -4,48 +4,37 @@
     meanvec = unit(mean(frame, dims=2))[:,1]
 end
 
-@userplot BlochPlot
-@recipe function f(bp::BlochPlot)
-    frame, xaxis, yaxis, axlim = bp.args
-    layout := @layout [scatter sphere]
+@userplot BlochScatter
+@recipe function f(bs::BlochScatter)
+    frame, xaxis, yaxis, axlim = bs.args
     x, y = project(frame, xaxis, yaxis)
-    
+    seriestype --> :scatter
+    aspect_ratio --> 1
+    xlims --> (-axlim, axlim)
+    ylims --> (-axlim, axlim)
+    x .- mean(x), y .- mean(y)
+end
+
+@userplot ShadowPlot
+@recipe function f(sp::ShadowPlot)
+    frame = sp.args[1]
     meanvec = unit(mean(frame, dims=2))[:,1]
     meanx = meanvec[1]
     meany = meanvec[2]
     meanz = meanvec[3]
-    
-    shadowalpha = 0.3
-    vectorcolor = :blue
-    
-    @series begin
-        subplot --> 1
-        seriestype --> :scatter
-        aspect_ratio --> 1
-        xlims --> (-axlim, axlim)
-        ylims --> (-axlim, axlim)
-        label --> "Spins"
-        x .- mean(x), y .- mean(y)
-    end
-    
-#     @series begin
-#         subplot --> 2
-#         seriestype --> :wireframe
-#         linecolor --> :blue
-#         sx, sy, sz
-#     end
-    
+
+    aspect_ratio --> 1
     xlims --> (-1, 1)
     ylims --> (-1, 1)
     zlims --> (-1, 1)
-    subplot --> 2
-    linecolor --> vectorcolor
-    
+
     @series begin
         [0, meanx], [0, meany], [0, meanz]
     end
-    
-    linealpha --> shadowalpha
+
+    # Draw the three shadows
+    linealpha := 0.5
+    primary := false
     @series begin
         [0, meanx], [0, meany], [-1, -1]
     end
@@ -56,3 +45,20 @@ end
         [0, meanx], [1, 1], [0, meanz]
     end
 end
+
+@userplot BlochPlot
+@recipe function f(bp::BlochPlot)
+    frame, xaxis, yaxis, axlim = bp.args
+    layout := @layout [scatter sphere]
+            
+    @series begin
+        subplot --> 1
+        BlochScatter((frame, xaxis, yaxis, axlim))
+    end
+
+    @series begin
+        subplot --> 2
+        ShadowPlot((frame,))
+    end 
+end
+
